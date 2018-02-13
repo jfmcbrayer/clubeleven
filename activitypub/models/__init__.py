928030f7aee06fa94a52e823286c5d35789220a3
Jason McBrayer <jmcbray@carcosa.net>
2018-02-13 12:54:50 -0500

    Working, but with old schema

--- activitypub/models/__init__.py
+++ activitypub/models/__init__.py
@@ -3,10 +3,10 @@
 from .core import Common
 from .core import PubObject
 from .core import Link
-# from .core import Actor
-# from .core import Collection
-# from .core import OrderedCollection
-# from .core import CollectionPage
-# from .core import OrderedCollectionPage
-# from .core import Activity
-# from .core import IntransitiveActivity
+from .core import Actor
+from .core import Collection
+from .core import OrderedCollection
+from .core import BaseActivity
+from .core import Activity
+from .core import IntransitiveActivity
+
