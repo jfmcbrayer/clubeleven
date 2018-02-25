a751366b7b1ddda992bcfe8f1c1ee41ef20436de
Jason McBrayer <jmcbray@carcosa.net>
2018-02-24 22:18:03 -0500

    Add the rest of the core model types.

--- clubeleven_models/models/__init__.py
+++ clubeleven_models/models/__init__.py
@@ -4,3 +4,7 @@ from .core import BasePost
 from .core import BaseActor
 from .core import User
 from .core import Persona
+from .core import Link
+from .core import Post
+from .core import Comment
+from .core import Reaction
--- clubeleven_models/models/core.py
+++ clubeleven_models/models/core.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.postgres import fields
+from django.core.serializers.json import DjangoJSONEncoder
 
 class BasePost(models.Model):
-    name = models.CharField(max_length=80, null=True)
+    name = models.CharField(max_length=140, null=True)
     media_type = models.CharField(max_length=80, null=True)
+    posted_by = models.ForeignKey('BaseActor', models.CASCADE, null=True)
+    icon = models.ImageField(null=True)
+    image = models.ImageField(null=True)
+    json = fields.JSONField(encoder = DjangoJSONEncoder,
+                            null=True)
 
 class BaseActor(models.Model):
     display_name = models.CharField(max_length=80)
@@ -25,3 +32,31 @@ class Persona(BaseActor):
                                                    ("MUT", "Friends only"),
                                                    ("LIST", "Listed people only"),))
     is_searchable = models.BooleanField(default=True)
+    avatar = models.ImageField(null=True)
+
+class Link(BasePost):
+    href = models.URLField(max_length=2048)
+    hreflang = models.CharField(max_length=8, null=True)
+    height = models.PositiveSmallIntegerField()
+    width = models.PositiveSmallIntegerField()
+    preview = models.TextField()
+
+class Post(BasePost):
+    content = models.TextField()
+    source = models.TextField()
+
+class Comment(Post):
+    conversation = models.ForeignKey(Post, models.CASCADE, related_name="comments")
+    parent = models.ForeignKey(Post, models.CASCADE, related_name="children")
+
+class Reaction(Comment):
+    type = models.CharField(max_length=8,
+                            choices=(("LIKE", "Like"),
+                                     ("DISLIKE", "Dislike"),
+                                     ("SEEN", "Seen")))
+    # This is weird, but emoji constructed with ZWJ characters can be quite long,
+    # though normal emoji are one character. I'm leaving this quite large, because
+    # the perfectly reasonable emojo "black woman kissing black woman" takes like
+    # 9 codepoints.
+    emojo = models.CharField(max_length=16, null=True)
+
