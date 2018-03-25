9b1f16252d54b4cde1163845a16af7c679ced909
Jason McBrayer <jmcbray@carcosa.net>
2018-03-24 22:37:06 -0400

    Just checkpointing before I start adding templates

--- clubeleven/views.py
+++ clubeleven/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views import View
+from django.shortcuts import render
 
 class APResponseMixin():
     """
@@ -7,5 +8,16 @@ class APResponseMixin():
     ActivityPub response or a normal HTML response.
     """
     def get(self, request, *args, **kwargs):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+@login_required
+class Inbox(View):
+    def get(self, request):
+        return render(request, "core/inbox.html")
+
+    def post(self, request):
         pass
 
