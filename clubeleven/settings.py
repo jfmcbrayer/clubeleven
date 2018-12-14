4124034c4fde748549144eab39389716fd544418
Jason McBrayer <jmcbray@carcosa.net>
2018-12-14 10:00:49 -0500

    Get all this basically working for dev again

--- clubeleven/settings.py
+++ clubeleven/settings.py
@@ -80,9 +80,8 @@ WSGI_APPLICATION = 'clubeleven.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'clubeleven',
-        'PASSWORD': 'minsk tort forgot scrape'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
---
+++ clubeleven/views/__init__.py
@@ -0,0 +1,1 @@
+from .Inbox import Inbox
