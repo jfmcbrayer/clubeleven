9a7ff3916310f18aa8edb106ef89844997c43a24
Jason McBrayer <jmcbray@carcosa.net>
2018-12-14 11:23:22 -0500

    Accept GET (html) and POST (json) to Inbox

--- clubeleven/views/Inbox.py
+++ clubeleven/views/Inbox.py
@@ -1,23 +1,36 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.conf import settings as django_settings
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.decorators.cache import never_cache, cache_page
+from django.urls import reverse
 from django.views import View
 from httpsig import HeaderVerifier
 
 class Inbox(View):
     def get(self, request):
         if request.META['HTTP_ACCEPT'].find('json') != -1:
-            return get_json(self, request)
+            return self.get_json(request)
         else:
-            return get_text(self, request)
+            return self.get_text(request)
     def post(self, request):
-        if request.META['HTTP_ACCEPT'].find('json') != -1:
-            return post_json(self, request)
+        if request.META['CONTENT_TYPE'].find('json') != -1:
+            return self.post_json(request)
         else:
-            return post_text(self, request)
+            return self.post_text(request)
 
     def post_json(self, request):
         # validate signature
-        signature_header = request.META.get("HTTP_SIGNATURE", "")
+        signature_header = request.META["HTTP_SIGNATURE"]
+        key_id, headers, signature = [x.strip().split("=")[1].strip('"')
+                                      for x in signature_header.split(",")]
 
         # store the activity
 
-        return signature_header
+        return HttpResponse(f'''keyID: {key_id}
+headers: {headers}
+signature: {signature}''')
+
+    def get_text(self, request):
+        return render(request, 'core/inbox.html')
+
