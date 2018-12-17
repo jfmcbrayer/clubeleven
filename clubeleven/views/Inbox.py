c3240fadd4f1d4b285caebb7e1ace64cbf670489
Jason McBrayer <jmcbray@carcosa.net>
2018-12-17 12:27:38 -0500

    Basic HTTP Signature checking

--- clubeleven/views/Inbox.py
+++ clubeleven/views/Inbox.py
@@ -10,7 +10,7 @@ from django.utils import timezone
 from clubeleven_models.models.core import InspectableMessage
 import sys
 import json
-
+import requests
 
 def inbox(request, username):
     if request.method == 'GET':
@@ -31,6 +31,8 @@ def post_json(request, username):
     # keyId="https://anticapitalist.party/users/gcupc#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="0XyzGimcjiZmuwiiQPRmQxPDBffYxxqJY6nGXPtZa1ooHy9oqA/Q0oTsUDGF9FIC84Ekbagqj15W2b+sPksK7ogTb/jMZr4x36zekMLlhTEcbqXOCJB35xJMLhDY5k68GMKeOZqSHCmBfNkQltcxSqFBJQRY+SDd+zdsUgQRQbEaKRFk/FIv2j+YhbLOHFNutD/WNgnR+PTR1e59FHQBI2Bdmx2Pby0MQz1wTRSjfpHYUpcQiiwWbUWnIAdCz5bTO1MpbOj1uzGAI8BsBixt9YW24fn+c86VVKlJ8AILPp1DFpEeELV1SyzLYPDRoctI6EObpD+FaAwjTAC+Ilytiw=="
     key_id, algorithm, headers, signature = [x.strip().split("=")[1].strip('"')
                                              for x in signature_header.split(",")]
+    sig_result = verify_signature(request, key_id, headers.split(" "), signature_header)
+
     # store the activity
     message = InspectableMessage()
     message.json = request.body.decode("UTF-8")
@@ -40,6 +42,8 @@ def post_json(request, username):
     message.digest = request.META.get("HTTP_DIGEST", None)
     message.content_type = request.META['CONTENT_TYPE']
     message.request_target = "(request target): " + request.method.lower() + " " + request.path
+    message.signature_checked = True
+    message.signature_check_passed = sig_result
     message.save()
 
     return HttpResponse("OK")
@@ -47,3 +51,24 @@ def post_json(request, username):
 def get_text(request, username):
     return render(request, 'core/inbox.html')
 
+def verify_signature(request, key_id, headers, signature):
+    # Fetch the key
+    req = requests.get(key_id, headers={"Accept": "application/ld+json"})
+    pubkey = req.json()["publicKey"]["publicKeyPem"]
+
+    #verify
+    request_headers =  {
+        "signature": signature,
+        "host": request.get_host(),
+        "date": request.META['HTTP_DATE'],
+        "digest": request.META.get("HTTP_DIGEST", None),
+        "content-type": request.META["CONTENT_TYPE"]
+    }
+    # TODO: regenerate and verify digest header
+    # TODO: check date stamp to avoid replay attacks
+    # TODO: digest only required if present.
+    # TODO: check attribution
+
+    hv = HeaderVerifier(request_headers, pubkey, headers, request.method,
+                        request.path, sign_header='Signature')
+    return hv.verify()
